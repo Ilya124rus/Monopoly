34200236ceded21be716b84c9624996d75e00eed
студ <you@example.com>
2018-07-04 04:14:55 +0800

    1-11

--- ui/Unit1.pas
+++ ui/Unit1.pas
@@ -7,15 +7,30 @@ uses
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
   UMGame,
-  Generics.Collections, Generics.Defaults;
+  Generics.Collections, Generics.Defaults, Vcl.Imaging.pngimage;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
     Label1: TLabel;
     Label2: TLabel;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Image6: TImage;
+    Image7: TImage;
+    Image8: TImage;
+    Image9: TImage;
+    Image10: TImage;
+    Image11: TImage;
+    Image12: TImage;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
+    procedure Image6Click(Sender: TObject);
+    procedure Image2Click(Sender: TObject);
   private
     MGame: TMGame;
   public
@@ -41,4 +56,19 @@ begin
   MGame := TMGame.create;
 end;
 
-end.
+eprocedure TForm1.Image1Click(Sender: TObject);
+begin
+
+eprocedure TForm1.Image6Click(Sender: TObject);
+begin
+
+eprocedure TForm1.Image2Click(Sender: TObject);
+begin
+
+end;
+
+nd;
+
+nd;
+
+nd.
